7cbeda8f5f11192de8fc8f0af029ae2c03594753
Saad <saad.ahmed212003@gmail.com>
2025-05-04 18:53:48 +0500

    Clean commit for PythonAnywhere13

--- feedback/settings.py
+++ feedback/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,11 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+54*19#4@+@c$-=ad-2f0f8aebytbrv&qrfd%l38+4a6z$0l-k'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', cast=bool)
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
