efcb1dbbb1af0bf4e66224e98a74de98debea2a7
Saad <saad.ahmed212003@gmail.com>
2025-05-04 16:03:56 +0500

    Clean commit for PythonAnywhere8

--- feedback/settings.py
+++ feedback/settings.py
@@ -128,6 +128,11 @@ DATABASES = {
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'reviews/static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- feedback/wsgi.py
+++ feedback/wsgi.py
@@ -7,23 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-import sys
 import os
-
-# Set path to your project directory
-project_home = '/home/SaadiCodes/NetConnect-HTTPS-website'
-if project_home not in sys.path:
-    sys.path.append(project_home)
-
-# Set the settings module
-os.environ['DJANGO_SETTINGS_MODULE'] = 'feedback.settings'  # adjust if your settings module name is different
-
-# Activate the virtualenv
-activate_this = '/home/SaadiCodes/NetConnect-HTTPS-website/venv/bin/activate_this.py'
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
-
-# Import Django
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feedback.settings')
+
+application = get_wsgi_application()
